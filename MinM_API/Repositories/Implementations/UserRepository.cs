da30f83daef613a6659ff769912a8c06de2f1ebf
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-03-07 17:22:25 +0200

    UserRepository realization

--- MinM_API/Repositories/Implementations/UserRepository.cs
+++ MinM_API/Repositories/Implementations/UserRepository.cs
@@ -1,17 +1,35 @@
 ﻿using MinM_API.Repositories.Interfaces;
+using System.Text.RegularExpressions;
 
 namespace MinM_API.Repositories.Implementations
 {
     public class UserRepository : IUserRepository
     {
-        public bool AreAllFieldsFilled<T>(T user) where T : class
-        {
-            throw new NotImplementedException();
-        }
-
         public bool IsValidEmail(string email)
         {
-            throw new NotImplementedException();
+            var emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, emailPattern);
+        }
+
+        public bool AreAllFieldsFilled<T>(T user) where T : class
+        {
+            var properties = user.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(user) as string;
+
+                if (value is null)
+                {
+                    continue;
+                }
+
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    return false; // Якщо поле містить лише пробіли або порожнє, повертаємо false
+                }
+            }
+            return true;
         }
     }
 }
