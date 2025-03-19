cc8dd8c629c82ce754a0f32c1bfc28d8817d7380
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-03-19 15:42:15 +0200

    Implemented AddCategory endpoint and service method (#2)

--- MinM_API/Repositories/Implementations/AdminService.cs
+++ MinM_API/Repositories/Implementations/AdminService.cs
@@ -1,8 +1,54 @@
-﻿using MinM_API.Repositories.Interfaces;
+﻿using MinM_API.Data;
+using MinM_API.Dtos;
+using MinM_API.Dtos.Category;
+using MinM_API.Models;
+using MinM_API.Repositories.Interfaces;
+using System.Net;
 
 namespace MinM_API.Repositories.Implementations
 {
-    public class AdminService : IAdminService
+    public class AdminService(DataContext context) : IAdminService
     {
+        public async Task<ServiceResponse<GetCategoryDto>> AddCategory(AddCategoryDto categoryDto)
+        {
+            var serviceResponse = new ServiceResponse<GetCategoryDto>();
+
+            try
+            {
+                var category = new Category()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = categoryDto.Name,
+                    Description = categoryDto.Description,
+                    ParentCategoryId = categoryDto.ParentCategoryId,
+                };
+
+                context.Categories.Add(category);
+                context.SaveChanges();
+
+                var getCategoryDto = new GetCategoryDto()
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    Description = category.Description,
+                    ParentCategoryId = category.ParentCategoryId,
+                };
+
+                serviceResponse.Data = getCategoryDto;
+                serviceResponse.IsSuccessful = true;
+                serviceResponse.Message = "Category successfully created";
+                serviceResponse.StatusCode = HttpStatusCode.OK;
+
+                return serviceResponse;
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Data = new GetCategoryDto();
+                serviceResponse.IsSuccessful = false;
+                serviceResponse.Message = ex.Message;
+                serviceResponse.StatusCode = HttpStatusCode.BadRequest;
+                return serviceResponse;
+            }
+        }
     }
 }
