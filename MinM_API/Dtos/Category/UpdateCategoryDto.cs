ec19c3de6e200edac350ee05c3d6a75bfcc88041
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-03-19 18:04:26 +0200

    Added UpdateCategoryDto

---
+++ MinM_API/Dtos/Category/UpdateCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MinM_API.Dtos.Category
+{
+    public class UpdateCategoryDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public string? ParentCategoryId { get; set; }
+    }
+}
