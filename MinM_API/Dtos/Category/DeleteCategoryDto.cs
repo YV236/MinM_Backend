138b18f4bed14dbf5409cc4236bc1ffb9172fb0f
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-03-19 18:55:36 +0200

    Added DeleteCategoryDto (#2)

---
+++ MinM_API/Dtos/Category/DeleteCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MinM_API.Dtos.Category
+{
+    public class DeleteCategoryDto
+    {
+        public string CategoryId { get; set; } = string.Empty;
+        public DeleteOption Option { get; set; } = DeleteOption.ReassignToParent;
+    }
+}
---
+++ MinM_API/Dtos/Category/DeleteOption.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MinM_API.Dtos.Category
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum DeleteOption
+    {
+        CascadeDelete,
+        ReassignToParent,
+        Orphan
+    }
+}
