d92e5a5469be57b348ea79b4d6a0792a9e23e9b5
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-03-28 13:39:25 +0200

    Decleration of ProductController and implementation of AddProduct endpoint (#3)

---
+++ MinM_API/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinM_API.Dtos;
+using MinM_API.Dtos.Products;
+using MinM_API.Services.Interfaces;
+
+namespace MinM_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController(IProductService productService) : ControllerBase
+    {
+        [HttpPost]
+        [Route("Create")]
+        public async Task<ActionResult<ServiceResponse<int>>> AddProduct(AddProductDto productDto)
+        {
+            var response = await productService.AddProduct(productDto);
+
+            return StatusCode((int)response.StatusCode, response);
+        }
+    }
+}
