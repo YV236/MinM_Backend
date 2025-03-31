a5a6c723110b4d2c111a1b5b5739d06fd33c23c3
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-03-31 16:06:33 +0300

    Created UpdateProductDto (#3)

---
+++ MinM_API/Dtos/Products/UpdateProductDto.cs
@@ -0,0 +1,16 @@
+﻿namespace MinM_API.Dtos.Products
+{
+    public class UpdateProductDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public List<string> ProductVariant { get; set; } = new();
+        public int UnitsInStock { get; set; }
+        public bool IsStock => UnitsInStock > 0;
+        public string CategoryId { get; set; } = string.Empty;
+        public string SKU { get; set; } = string.Empty;
+        public List<string> ImageUrls { get; set; } = new();
+    }
+}
