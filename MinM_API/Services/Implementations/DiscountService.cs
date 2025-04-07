9645e3037bf988de7af3ad5cd4ad4f7770c69c3f
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-04-07 15:45:34 +0300

    Implementation of AddDiscount method (#4)

--- MinM_API/Services/Implementations/DiscountService.cs
+++ MinM_API/Services/Implementations/DiscountService.cs
@@ -1,8 +1,14 @@
-﻿using MinM_API.Services.Interfaces;
+﻿using MinM_API.Dtos;
+using MinM_API.Dtos.Discount;
+using MinM_API.Services.Interfaces;
 
 namespace MinM_API.Services.Implementations
 {
     public class DiscountService : IDiscountService
     {
+        public Task<ServiceResponse<int>> AddDiscount(AddDiscountDto dto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MinM_API/Services/Interfaces/IDiscountService.cs
+++ MinM_API/Services/Interfaces/IDiscountService.cs
@@ -1,6 +1,10 @@
-﻿namespace MinM_API.Services.Interfaces
+﻿using MinM_API.Dtos;
+using MinM_API.Dtos.Discount;
+
+namespace MinM_API.Services.Interfaces
 {
     public interface IDiscountService
     {
+        Task<ServiceResponse<int>> AddDiscount(AddDiscountDto dto);
     }
 }
