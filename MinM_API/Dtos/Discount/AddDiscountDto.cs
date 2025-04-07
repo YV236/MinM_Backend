60ff155923c17c7f8d4b26b70a180e63e1887db4
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-04-07 15:44:36 +0300

    AddDiscountDto (#4)

---
+++ MinM_API/Dtos/Discount/AddDiscountDto.cs
@@ -0,0 +1,14 @@
+﻿using MinM_API.Dtos.Products;
+using MinM_API.Models;
+
+namespace MinM_API.Dtos.Discount
+{
+    public class AddDiscountDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal DiscountPercentage { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public virtual List<GetProductDto> Products { get; set; } = [];
+    }
+}
