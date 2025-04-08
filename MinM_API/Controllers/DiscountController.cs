9412045691ee62fb6a4caa0aa765ee0abecfe15a
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-04-08 15:25:55 +0300

    AddDiscount endpoint implementation (#4)

--- MinM_API/Controllers/DiscountController.cs
+++ MinM_API/Controllers/DiscountController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MinM_API.Dtos;
+using MinM_API.Dtos.Discount;
+using MinM_API.Services.Interfaces;
 
 namespace MinM_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DiscountController : ControllerBase
+    public class DiscountController(IDiscountService discountService) : ControllerBase
     {
+        [HttpPost]
+        [Route("Create")]
+        public async Task<ActionResult<ServiceResponse<int>>> AddDiscount(AddDiscountDto discountDto)
+        {
+            var response = await discountService.AddDiscount(discountDto);
+
+            return StatusCode((int)response.StatusCode, response);
+        }
     }
 }
