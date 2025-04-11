be8c298a38ae3ba0a8511af812a56f96bdd52de6
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-04-11 16:08:30 +0300

    Created GetDiscountDto (#4)

---
+++ MinM_API/Dtos/Discount/GetDiscountDto.cs
@@ -0,0 +1,14 @@
+﻿using MinM_API.Dtos.Products;
+
+namespace MinM_API.Dtos.Discount
+{
+    public class GetDiscountDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal DiscountPercentage { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<GetProductDto> Products { get; set; } = [];
+    }
+}
