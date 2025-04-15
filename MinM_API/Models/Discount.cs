d3d6de5d9379d2dcfcec168b3b3ca8cad8da9e12
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-04-15 17:36:34 +0300

    Discount Model update (#4)

--- MinM_API/Models/Discount.cs
+++ MinM_API/Models/Discount.cs
@@ -7,6 +7,10 @@
         public decimal DiscountPercentage { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+
+        public bool IsActive { get; set; } = true;
+        public bool RemoveAfterExpiration { get; set; } = false;
+
         public virtual List<Product> Products { get; set; } = [];
     }
 }
