c7401c0c701318c6966fdf7bf74e29b040157d91
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-04-30 13:15:13 +0200

    ProductVariant model declaration (#5)

---
+++ MinM_API/Models/ProductVariant.cs
@@ -0,0 +1,19 @@
+﻿namespace MinM_API.Models
+{
+    public class ProductVariant
+    {
+        public string Id { get; set; }
+        public string ProductId { get; set; }
+        public virtual List<Product> Product { get; set; } = null!;
+
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int UnitsInStock { get; set; }
+        public bool IsStock { get; set; }
+
+        public string? DiscountId { get; set; }
+        public virtual Discount? Discount { get; set; }
+        public decimal? DiscountPrice { get; set; }
+        public bool IsDiscounted { get; set; } = false;
+    }
+}
