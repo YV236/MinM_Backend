583ed8bfd435e55bffba1e0ee0c35a6f18cd000c
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-05-01 14:10:36 +0200

    ProductVariantDto declaration (#5)

--- MinM_API/Dtos/Products/AddProductDto.cs
+++ MinM_API/Dtos/Products/AddProductDto.cs
@@ -4,10 +4,7 @@
     {
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-        public List<string> ProductVariant { get; set; } = [];
-        public int UnitsInStock { get; set; }
-        public bool IsStock => UnitsInStock > 0;
+        public List<ProductVariantDto> ProductVariant { get; set; } = [];
         public string CategoryId { get; set; } = string.Empty;
         public string SKU { get; set; } = string.Empty;
         public List<string> ImageUrls { get; set; } = [];
---
+++ MinM_API/Dtos/Products/ProductVariantDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MinM_API.Dtos.Products
+{
+    public class ProductVariantDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int UnitsInStock { get; set; }
+        public bool IsStock => UnitsInStock > 0;
+    }
+}
