12cb17dadc3f8bf1fa26ff779768bfcf978fa597
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-05-01 15:38:30 +0200

    UpdateProductVariantDto declaration and implementation (#5)

---
+++ MinM_API/Dtos/ProductVariant/UpdateProductVariantDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MinM_API.Dtos.ProductVariant
+{
+    public class UpdateProductVariantDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int UnitsInStock { get; set; }
+        public bool IsStock => UnitsInStock > 0;
+    }
+}
--- MinM_API/Dtos/Products/UpdateProductDto.cs
+++ MinM_API/Dtos/Products/UpdateProductDto.cs
@@ -1,14 +1,13 @@
-﻿namespace MinM_API.Dtos.Products
+﻿using MinM_API.Dtos.ProductVariant;
+
+namespace MinM_API.Dtos.Products
 {
     public class UpdateProductDto
     {
         public string Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-        public List<string> ProductVariant { get; set; } = [];
-        public int UnitsInStock { get; set; }
-        public bool IsStock => UnitsInStock > 0;
+        public List<UpdateProductVariantDto> ProductVariants { get; set; } = [];
         public string CategoryId { get; set; } = string.Empty;
         public string SKU { get; set; } = string.Empty;
         public List<string> ImageUrls { get; set; } = [];
