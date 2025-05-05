22c641d033aeaac2f6eff58132843bc83e9e0160
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-05-05 13:15:11 +0200

    SlugExtensionUpdate (#2)

--- MinM_API/Extension/SlugExtension.cs
+++ MinM_API/Extension/SlugExtension.cs
@@ -1,15 +1,101 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
-namespace MinM_API.Extension
+public static class SlugExtension
 {
-    public class SlugExtension
+    private static readonly Dictionary<char, string> ConvertedLetters = new Dictionary<char, string>
     {
-        public static string GenerateSlug(string input)
+        ['а'] = "a",
+        ['б'] = "b",
+        ['в'] = "v",
+        ['г'] = "h",
+        ['ґ'] = "g",
+        ['д'] = "d",
+        ['е'] = "e",
+        ['є'] = "ie",
+        ['ж'] = "zh",
+        ['з'] = "z",
+        ['и'] = "y",
+        ['і'] = "i",
+        ['ї'] = "i",
+        ['й'] = "i",
+        ['к'] = "k",
+        ['л'] = "l",
+        ['м'] = "m",
+        ['н'] = "n",
+        ['о'] = "o",
+        ['п'] = "p",
+        ['р'] = "r",
+        ['с'] = "s",
+        ['т'] = "t",
+        ['у'] = "u",
+        ['ф'] = "f",
+        ['х'] = "kh",
+        ['ц'] = "ts",
+        ['ч'] = "ch",
+        ['ш'] = "sh",
+        ['щ'] = "shch",
+        ['ь'] = "",
+        ['ю'] = "iu",
+        ['я'] = "ia",
+
+        ['А'] = "A",
+        ['Б'] = "B",
+        ['В'] = "V",
+        ['Г'] = "H",
+        ['Ґ'] = "G",
+        ['Д'] = "D",
+        ['Е'] = "E",
+        ['Є'] = "Ye",
+        ['Ж'] = "Zh",
+        ['З'] = "Z",
+        ['И'] = "Y",
+        ['І'] = "I",
+        ['Ї'] = "Yi",
+        ['Й'] = "Y",
+        ['К'] = "K",
+        ['Л'] = "L",
+        ['М'] = "M",
+        ['Н'] = "N",
+        ['О'] = "O",
+        ['П'] = "P",
+        ['Р'] = "R",
+        ['С'] = "S",
+        ['Т'] = "T",
+        ['У'] = "U",
+        ['Ф'] = "F",
+        ['Х'] = "Kh",
+        ['Ц'] = "Ts",
+        ['Ч'] = "Ch",
+        ['Ш'] = "Sh",
+        ['Щ'] = "Shch",
+        ['Ь'] = "",
+        ['Ю'] = "Iu",
+        ['Я'] = "Ia"
+    };
+
+    public static string GenerateSlug(string input)
+    {
+        string transliterated = TransliterateCyrillicToLatin(input);
+
+        transliterated = transliterated.ToLowerInvariant();
+
+        transliterated = Regex.Replace(transliterated, @"\s+", "-");
+
+        transliterated = Regex.Replace(transliterated, @"[^a-z0-9\-]", "");
+
+        return transliterated.Trim('-');
+    }
+
+    private static string TransliterateCyrillicToLatin(string input)
+    {
+        var result = new StringBuilder();
+
+        foreach (char c in input)
         {
-            var normalized = input.ToLowerInvariant();
-            normalized = Regex.Replace(normalized, @"[^a-z0-9\s-]", "");
-            normalized = Regex.Replace(normalized, @"\s+", "-");
-            return normalized.Trim('-');
+            result.Append(ConvertedLetters.ContainsKey(c) ? ConvertedLetters[c] : c.ToString());
         }
+
+        return result.ToString();
     }
 }
