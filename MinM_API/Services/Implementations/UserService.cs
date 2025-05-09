969067966f58bfc9f294685b5d4d1ad0d83bc542
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-05-09 16:34:10 +0200

    ResponseFactory implementation into UserService (#7)

--- MinM_API/Services/Implementations/UserService.cs
+++ MinM_API/Services/Implementations/UserService.cs
@@ -16,38 +16,26 @@ namespace MinM_API.Services.Implementations
     {
         public async Task<ServiceResponse<int>> Register(UserRegisterDto userRegisterDto)
         {
-            var serviceResponse = new ServiceResponse<int>();
 
             if (!userRepository.AreAllFieldsFilled(userRegisterDto))
             {
-                serviceResponse.Data = 0;
-                serviceResponse.IsSuccessful = false;
-                serviceResponse.Message = "Error while registering. Some of the properties may be filled incorrectly";
-                serviceResponse.StatusCode = HttpStatusCode.UnprocessableEntity;
-
-                return serviceResponse;
+                return ResponseFactory.Error(0,
+                    "Error while registering. Some of the properties may be filled incorrectly",
+                    HttpStatusCode.UnprocessableEntity);
             }
 
             if (!userRepository.IsValidEmail(userRegisterDto.Email))
             {
-                serviceResponse.Data = 0;
-                serviceResponse.IsSuccessful = false;
-                serviceResponse.Message = $"Registration failed." +
-                    $" The email '{userRegisterDto.Email}' must include '@' and a domain such as '.com' or '.pl'.";
-                serviceResponse.StatusCode = HttpStatusCode.UnprocessableEntity;
-
-                return serviceResponse;
+                return ResponseFactory.Error(0,
+                    $"Registration failed. The email '{userRegisterDto.Email}' must include '@' and a domain such as '.com' or '.pl'.",
+                    HttpStatusCode.UnprocessableEntity);
             }
 
             if (userRegisterDto.PhoneNumber.Any(c => !char.IsDigit(c)) || userRegisterDto.PhoneNumber.Length < 9)
             {
-                serviceResponse.Data = 0;
-                serviceResponse.IsSuccessful = false;
-                serviceResponse.Message = $"Error while registering. Phone number" +
-                    $" '{userRegisterDto.PhoneNumber}' must contain numbers only. And contain at least 9 digits";
-                serviceResponse.StatusCode = HttpStatusCode.UnprocessableEntity;
-
-                return serviceResponse;
+                return ResponseFactory.Error(0,
+                    $"Error while registering. Phone number '{userRegisterDto.PhoneNumber}' must contain numbers only. And contain at least 9 digits",
+                    HttpStatusCode.UnprocessableEntity);
             }
 
             try
@@ -76,58 +64,36 @@ namespace MinM_API.Services.Implementations
                 user.AddressId = user.Address.Id;
 
                 var result = await userManager.CreateAsync(user, userRegisterDto.Password);
+
                 if (!result.Succeeded)
                 {
-                    serviceResponse.Data = 0;
-                    serviceResponse.IsSuccessful = false;
-                    serviceResponse.Message = string.Join(", ", result.Errors.Select(e => e.Description));
-                    serviceResponse.StatusCode = HttpStatusCode.BadRequest;
-
-                    return serviceResponse;
+                    return ResponseFactory.Error(0, string.Join(", ", result.Errors.Select(e => e.Description)));
                 }
 
                 var addRoleResult = await userManager.AddToRoleAsync(user, "User");
+
                 if (!addRoleResult.Succeeded)
                 {
-                    serviceResponse.Data = 0;
-                    serviceResponse.IsSuccessful = false;
-                    serviceResponse.Message = "Failed to assign role to user.";
-                    serviceResponse.StatusCode = HttpStatusCode.BadRequest;
-                    return serviceResponse;
+                    return ResponseFactory.Error(0, "Failed to assign role to user.");
                 }
 
-                serviceResponse.Data = 1;
-                serviceResponse.IsSuccessful = true;
-                serviceResponse.Message = "User registered successfully";
-
-                return serviceResponse;
+                return ResponseFactory.Success(1, "User registered successfully");
             }
             catch (Exception ex)
             {
-                serviceResponse.Data = 0;
-                serviceResponse.IsSuccessful = false;
-                serviceResponse.Message = ex.Message;
-                serviceResponse.StatusCode = HttpStatusCode.BadRequest;
-                return serviceResponse;
+                return ResponseFactory.Error(0, "Internal error");
             }
         }
 
         public async Task<ServiceResponse<GetUserDto>> GetUserInfo(ClaimsPrincipal user)
         {
-            var serviceResponse = new ServiceResponse<GetUserDto>();
-
             try
             {
                 var getUser = await userRepository.FindUser(user, context);
 
                 if (getUser == null)
                 {
-                    serviceResponse.Data = null;
-                    serviceResponse.IsSuccessful = false;
-                    serviceResponse.Message = "Unable to find the user.";
-                    serviceResponse.StatusCode = HttpStatusCode.NotFound;
-
-                    return serviceResponse;
+                    return ResponseFactory.Error(new GetUserDto(), "Unable to find the user.", HttpStatusCode.NotFound);
                 }
 
                 var userDto = new GetUserDto()
@@ -148,49 +114,36 @@ namespace MinM_API.Services.Implementations
                     },
                     PhoneNumber = getUser.PhoneNumber,
                 };
-                serviceResponse.Data = userDto;
-                serviceResponse.IsSuccessful = true;
+
+                return ResponseFactory.Success(userDto);
             }
             catch (Exception ex)
             {
-                serviceResponse.Data = null;
-                serviceResponse.IsSuccessful = false;
-                serviceResponse.Message = ex.Message;
-                serviceResponse.StatusCode = HttpStatusCode.BadRequest;
-                return serviceResponse;
+                return ResponseFactory.Error(new GetUserDto(), "Internal error");
             }
-
-            return serviceResponse;
         }
 
         public async Task<ServiceResponse<GetUserDto>> UpdateUserInfo(ClaimsPrincipal user, UpdateUserDto userUpdateDto)
         {
-            var serviceResponse = new ServiceResponse<GetUserDto>();
             var getUser = await userRepository.FindUser(user, context);
 
             if (getUser == null)
             {
-                serviceResponse.IsSuccessful = false;
-                serviceResponse.Message = "User not found.";
-                serviceResponse.StatusCode = HttpStatusCode.NotFound;
-                return serviceResponse;
+                return ResponseFactory.Error(new GetUserDto(), "User not found.", HttpStatusCode.NotFound);
             }
 
             if (!userRepository.AreAllFieldsFilled(userUpdateDto))
             {
-                serviceResponse.IsSuccessful = false;
-                serviceResponse.Message = "Error while updating. Some of the properties may be filled incorrectly.";
-                serviceResponse.StatusCode = HttpStatusCode.UnprocessableEntity;
-                return serviceResponse;
+                return ResponseFactory.Error(new GetUserDto(),
+                    "Error while updating. Some of the properties may be filled incorrectly.",
+                    HttpStatusCode.UnprocessableEntity);
             }
 
             if (userUpdateDto.PhoneNumber.Any(c => !char.IsDigit(c)) || userUpdateDto.PhoneNumber.Length < 9)
             {
-                serviceResponse.IsSuccessful = false;
-                serviceResponse.Message = $"Error while registering. " +
-                    $"Phone number '{userUpdateDto.PhoneNumber}' must contain numbers only and have at least 9 digits.";
-                serviceResponse.StatusCode = HttpStatusCode.UnprocessableEntity;
-                return serviceResponse;
+                return ResponseFactory.Error(new GetUserDto(),
+                    $"Error while registering. Phone number '{userUpdateDto.PhoneNumber}' must contain numbers only and have at least 9 digits.",
+                    HttpStatusCode.UnprocessableEntity);
             }
 
             getUser.UserFirstName = userUpdateDto.UserFirstName;
@@ -210,7 +163,7 @@ namespace MinM_API.Services.Implementations
 
             await context.SaveChangesAsync();
 
-            serviceResponse.Data = new GetUserDto()
+            return ResponseFactory.Success(new GetUserDto()
             {
                 UserFirstName = getUser.UserFirstName,
                 UserLastName = getUser.UserLastName,
@@ -225,11 +178,8 @@ namespace MinM_API.Services.Implementations
                     Country = getUser.Address!.Country,
                 },
                 PhoneNumber = getUser.PhoneNumber,
-            };
-
-            serviceResponse.IsSuccessful = true;
-            serviceResponse.Message = "The data successfully updated";
-            return serviceResponse;
+            },
+            "The data successfully updated");
         }
     }
 }
