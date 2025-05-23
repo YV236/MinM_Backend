0e9adee2df70718bd5b5faf4d841b871cbd8fef2
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-05-23 15:27:44 +0200

    Implement ILogger logging in UserService (#9)

--- MinM_API/Services/Implementations/UserService.cs
+++ MinM_API/Services/Implementations/UserService.cs
@@ -14,13 +14,17 @@ using System.Text.RegularExpressions;
 
 namespace MinM_API.Services.Implementations
 {
-    public class UserService(IUserRepository userRepository, UserManager<User> userManager, DataContext context, UserMapper mapper) : IUserService
+    public class UserService(IUserRepository userRepository, UserManager<User> userManager,
+        DataContext context, UserMapper mapper, ILogger<UserService> logger) : IUserService
     {
         public async Task<ServiceResponse<int>> Register(UserRegisterDto userRegisterDto)
         {
+            var emptyFields = GetEmptyStringFields(userRegisterDto);
 
-            if (!AreAllFieldsFilled(userRegisterDto))
+            if (emptyFields.Any())
             {
+                var fieldList = string.Join(", ", emptyFields);
+                logger.LogInformation($"Fail: Registration error. The following fields are missing or invalid: {fieldList}");
                 return ResponseFactory.Error(0,
                     "Error while registering. Some of the properties may be filled incorrectly",
                     HttpStatusCode.UnprocessableEntity);
@@ -28,6 +32,7 @@ namespace MinM_API.Services.Implementations
 
             if (!IsValidEmail(userRegisterDto.Email))
             {
+                logger.LogInformation("Fail: 'Email' field is missing or not in correct format. Email: {Email}", userRegisterDto.Email);
                 return ResponseFactory.Error(0,
                     $"Registration failed. The email '{userRegisterDto.Email}' must include '@' and a domain such as '.com' or '.pl'.",
                     HttpStatusCode.UnprocessableEntity);
@@ -35,6 +40,7 @@ namespace MinM_API.Services.Implementations
 
             if (userRegisterDto.PhoneNumber.Any(c => !char.IsDigit(c)) || userRegisterDto.PhoneNumber.Length < 9)
             {
+                logger.LogInformation("Fail: 'Phone number' field is missing or not in correct format. Phone number: {Number}", userRegisterDto.PhoneNumber);
                 return ResponseFactory.Error(0,
                     $"Error while registering. Phone number '{userRegisterDto.PhoneNumber}' must contain numbers only. And contain at least 9 digits",
                     HttpStatusCode.UnprocessableEntity);
@@ -69,20 +75,26 @@ namespace MinM_API.Services.Implementations
 
                 if (!result.Succeeded)
                 {
-                    return ResponseFactory.Error(0, string.Join(", ", result.Errors.Select(e => e.Description)));
+                    logger.LogInformation("Fail: Fail while creating User. Message: {Message}", string.Join(", ", result.Errors.Select(e => e.Description)));
+                    return ResponseFactory.Error(0, "Failed to create user");
                 }
 
                 var addRoleResult = await userManager.AddToRoleAsync(user, "User");
 
                 if (!addRoleResult.Succeeded)
                 {
-                    return ResponseFactory.Error(0, "Failed to assign role to user.");
+                    var errorDescriptions = string.Join("; ", addRoleResult.Errors.Select(e => e.Description));
+
+                    logger.LogError("Fail: Failed to assign role 'User' to user {UserId}. Message: {Message}", user.Id, errorDescriptions);
+
+                    return ResponseFactory.Error(0, "Failed to assign role to user. Please contact support.");
                 }
 
                 return ResponseFactory.Success(1, "User registered successfully");
             }
             catch (Exception ex)
             {
+                logger.LogError(ex, "Fail: Error while registration. Input data: {@UserRegisterDto}", userRegisterDto);
                 return ResponseFactory.Error(0, "Internal error");
             }
         }
@@ -104,42 +116,56 @@ namespace MinM_API.Services.Implementations
             }
             catch (Exception ex)
             {
+                logger.LogError(ex, "Fail: Error while to retrieving products from database");
                 return ResponseFactory.Error(new GetUserDto(), "Internal error");
             }
         }
 
         public async Task<ServiceResponse<GetUserDto>> UpdateUserInfo(ClaimsPrincipal user, UpdateUserDto userUpdateDto)
         {
-            var getUser = await userRepository.FindUser(user, context);
-
-            if (getUser == null)
+            try
             {
-                return ResponseFactory.Error(new GetUserDto(), "User not found.", HttpStatusCode.NotFound);
-            }
+                var getUser = await userRepository.FindUser(user, context);
 
-            if (!AreAllFieldsFilled(userUpdateDto))
+                if (getUser == null)
+                {
+                    return ResponseFactory.Error(new GetUserDto(), "User not found.", HttpStatusCode.NotFound);
+                }
+
+                var emptyFields = GetEmptyStringFields(userUpdateDto);
+
+                if (emptyFields.Any())
+                {
+                    var fieldList = string.Join(", ", emptyFields);
+                    logger.LogInformation($"Fail: The following fields are missing or invalid: {fieldList}");
+
+                    return ResponseFactory.Error(new GetUserDto(),
+                        "Error while updating data. Some of the properties may be filled incorrectly",
+                        HttpStatusCode.UnprocessableEntity);
+                }
+
+                if (userUpdateDto.PhoneNumber.Any(c => !char.IsDigit(c)) || userUpdateDto.PhoneNumber.Length < 9)
+                {
+                    return ResponseFactory.Error(new GetUserDto(),
+                        $"Error while registering. Phone number '{userUpdateDto.PhoneNumber}' must contain numbers only and have at least 9 digits.",
+                        HttpStatusCode.UnprocessableEntity);
+                }
+
+                mapper.UpdateUserDtoToUserModel(userUpdateDto, getUser);
+                mapper.UpdateAddressDtoToAddress(userUpdateDto.AddressDto, getUser.Address);
+
+                context.Users.Update(getUser);
+
+                await context.SaveChangesAsync();
+
+                return ResponseFactory.Success(mapper.UserToGetUserDto(getUser),
+                "The data successfully updated");
+            }
+            catch (Exception ex)
             {
-                return ResponseFactory.Error(new GetUserDto(),
-                    "Error while updating. Some of the properties may be filled incorrectly.",
-                    HttpStatusCode.UnprocessableEntity);
+                logger.LogError(ex, "Fail: Error while updating data. Input data: {@UserUpdaterDto}", userUpdateDto);
+                return ResponseFactory.Error(new GetUserDto(), "Internal error");
             }
-
-            if (userUpdateDto.PhoneNumber.Any(c => !char.IsDigit(c)) || userUpdateDto.PhoneNumber.Length < 9)
-            {
-                return ResponseFactory.Error(new GetUserDto(),
-                    $"Error while registering. Phone number '{userUpdateDto.PhoneNumber}' must contain numbers only and have at least 9 digits.",
-                    HttpStatusCode.UnprocessableEntity);
-            }
-
-            mapper.UpdateUserDtoToUserModel(userUpdateDto, getUser);
-            mapper.UpdateAddressDtoToAddress(userUpdateDto.AddressDto, getUser.Address);
-
-            context.Users.Update(getUser);
-
-            await context.SaveChangesAsync();
-
-            return ResponseFactory.Success(mapper.UserToGetUserDto(getUser),
-            "The data successfully updated");
         }
 
         private static bool IsValidEmail(string email)
@@ -148,23 +174,26 @@ namespace MinM_API.Services.Implementations
             return Regex.IsMatch(email, emailPattern);
         }
 
-        private static bool AreAllFieldsFilled<T>(T user) where T : class
+        private static List<string> GetEmptyStringFields<T>(T obj) where T : class
         {
-            var properties = user.GetType().GetProperties();
+            var emptyFields = new List<string>();
+
+            var properties = obj.GetType().GetProperties();
 
             foreach (var property in properties)
             {
-                if (property.GetValue(user) is not string value)
+                if (property.GetValue(obj) is not string value)
                 {
                     continue;
                 }
 
                 if (string.IsNullOrWhiteSpace(value))
                 {
-                    return false;
+                    emptyFields.Add(property.Name);
                 }
             }
-            return true;
+
+            return emptyFields;
         }
     }
 }
