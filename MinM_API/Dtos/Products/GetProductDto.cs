17bd6b61ce10dfb55aa74ce5c1ef6e85df3c90af
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-06-02 15:18:25 +0200

    GetProductDto update

--- MinM_API/Dtos/Products/GetProductDto.cs
+++ MinM_API/Dtos/Products/GetProductDto.cs
@@ -11,6 +11,7 @@ namespace MinM_API.Dtos.Products
         public List<GetProductVariantDto> ProductVariants { get; set; } = [];
         public string DiscountId { get; set; } = string.Empty;
         public bool? IsSeasonal { get; set; }
+        public bool? IsDiscounted { get; set; }
         public string CategoryId { get; set; }
         public string CategoryName { get; set; } = string.Empty;
         public string SKU { get; set; } = string.Empty; // Product article
