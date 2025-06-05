b93d602b0a6473bfc001730a2702550dba831e98
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-06-05 15:48:55 +0200

    SlugInSeasonTable migration (#11)

---
+++ MinM_API/Migrations/20250605133546_SlugInSeasonTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinM_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SlugInSeasonTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Seasons",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seasons_Slug",
+                table: "Seasons",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Seasons_Slug",
+                table: "Seasons");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Seasons");
+        }
+    }
+}
--- MinM_API/Models/Season.cs
+++ MinM_API/Models/Season.cs
@@ -1,9 +1,13 @@
-﻿namespace MinM_API.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinM_API.Models
 {
+    [Index(nameof(Slug), IsUnique = true)]
     public class Season
     {
         public string Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string Slug { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public virtual List<Product> Products { get; set; } = [];
