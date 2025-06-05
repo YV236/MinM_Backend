d76c05a2a6b8dbe2e208cf03109a2a67fa5ac03b
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-06-05 14:41:05 +0200

    UpdateProductDto update

--- MinM_API/Dtos/Products/UpdateProductDto.cs
+++ MinM_API/Dtos/Products/UpdateProductDto.cs
@@ -5,18 +5,13 @@ namespace MinM_API.Dtos.Products
 {
     public class UpdateProductDto
     {
-        public string Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-
-        [FromForm(Name = "ProductVariantsJson")]
-        public string ProductVariantsJson { get; set; } = string.Empty;
-
-        public string CategoryId { get; set; } = string.Empty;
-        public string SKU { get; set; } = string.Empty;
-
-        public List<string> ExistingImageUrls { get; set; } = [];
-
-        public List<IFormFile> NewImages { get; set; } = [];
+        [FromForm] public string Id { get; set; } = string.Empty;
+        [FromForm] public string Name { get; set; } = string.Empty;
+        [FromForm] public string Description { get; set; } = string.Empty;
+        [FromForm(Name = "ProductVariantsJson")] public string ProductVariantsJson { get; set; } = string.Empty;
+        [FromForm] public string CategoryId { get; set; } = string.Empty;
+        [FromForm] public string SKU { get; set; } = string.Empty;
+        [FromForm] public List<string> ExistingImageUrls { get; set; } = new();
+        [FromForm] public List<IFormFile> NewImages { get; set; } = new();
     }
 }
