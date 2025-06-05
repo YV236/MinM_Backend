75a6f070534469b6bbd2c0a786be451082e09f5a
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-06-05 15:03:43 +0200

    SeasonController declaration (#11)

---
+++ MinM_API/Controllers/SeasonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinM_API.Services.Interfaces;
+
+namespace MinM_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SeasonController(ISeasonService seasonService) : ControllerBase
+    {
+    }
+}
