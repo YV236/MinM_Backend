778b68f10dcc563d19c83cee59d16bfa1cd8f2b4
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-06-05 15:51:34 +0200

    SeasonService implementation (#11)

--- MinM_API/Controllers/SeasonController.cs
+++ MinM_API/Controllers/SeasonController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MinM_API.Dtos;
+using MinM_API.Services.Implementations;
 using MinM_API.Services.Interfaces;
+using MinM_API.Dtos.Season;
 
 namespace MinM_API.Controllers
 {
@@ -8,5 +12,14 @@ namespace MinM_API.Controllers
     [Route("api/[controller]")]
     public class SeasonController(ISeasonService seasonService) : ControllerBase
     {
+        [HttpPost]
+        [Route("create")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<ServiceResponse<int>>> AddSeason(AddSeasonDto seasonDto)
+        {
+            var response = await seasonService.AddSeason(seasonDto);
+
+            return StatusCode((int)response.StatusCode, response);
+        }
     }
 }
--- MinM_API/Services/Implementations/SeasonService.cs
+++ MinM_API/Services/Implementations/SeasonService.cs
@@ -1,14 +1,56 @@
-﻿using MinM_API.Dtos;
+﻿using Microsoft.EntityFrameworkCore;
+using MinM_API.Data;
+using MinM_API.Dtos;
 using MinM_API.Dtos.Season;
+using MinM_API.Extension;
+using MinM_API.Mappers;
+using MinM_API.Models;
 using MinM_API.Services.Interfaces;
+using System.Net;
 
 namespace MinM_API.Services.Implementations
 {
-    public class SeasonService : ISeasonService
+    public class SeasonService(DataContext context, ILogger<SeasonService> logger) : ISeasonService
     {
-        public Task<ServiceResponse<int>> AddSeason(AddSeasonDto addSeasonDto)
+        public async Task<ServiceResponse<int>> AddSeason(AddSeasonDto addSeasonDto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var season = new Season()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = addSeasonDto.Name,
+                    Slug = SlugExtension.GenerateSlug(addSeasonDto.Name),
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now,
+                };
+
+                var productList = await context.Products
+                   .Where(p => addSeasonDto.ProductIds.Contains(p.Id))
+                   .ToListAsync();
+
+                if (productList == null)
+                {
+                    logger.LogInformation("Fail: No products found in database");
+                    return ResponseFactory.Error(0, "Product not found", HttpStatusCode.NotFound);
+                }
+
+                foreach (var product in productList)
+                {
+                    product.Season = season;
+                    product.SeasonId = season.Id;
+                    product.IsSeasonal = true;
+                }
+
+                await context.SaveChangesAsync();
+
+                return ResponseFactory.Success(1, "Successful season creation");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Fail: Error while adding season. Name: {SeasonName}", addSeasonDto.Name);
+                return ResponseFactory.Error(0, "Internal error");
+            }
         }
 
         public Task<ServiceResponse<List<GetSeasonDto>>> GetAllSeasons()
