14b470c5d887d2bdab2511757eacb93452c23ea2
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-06-27 13:47:33 +0200

    TokenResponse DTO declaration (#16)

---
+++ MinM_API/Dtos/TokenResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MinM_API.Dtos
+{
+    public class TokenResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
