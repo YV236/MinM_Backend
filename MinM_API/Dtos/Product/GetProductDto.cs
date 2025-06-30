689a39219b8d7338b4497c6527ba8aff2ceead1e
PKirill-106 <p.kirill.design@gmail.com>
2025-06-30 15:39:46 +0200

    feat: isNew property in getProductDto

--- MinM_API/Dtos/Product/GetProductDto.cs
+++ MinM_API/Dtos/Product/GetProductDto.cs
@@ -12,6 +12,7 @@ namespace MinM_API.Dtos.Product
         public string DiscountId { get; set; } = string.Empty;
         public bool? IsSeasonal { get; set; }
         public bool? IsDiscounted { get; set; }
+        public bool? IsNew { get; set; }
         public string CategoryId { get; set; }
         public string CategoryName { get; set; } = string.Empty;
         public string SKU { get; set; } = string.Empty; // Product article
