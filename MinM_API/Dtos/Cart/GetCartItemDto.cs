6caf98c8a1a6698dcfb484b05cf71d6d7893169f
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-07-20 11:45:16 +0200

    Fixed GetCartItemDto (#19)

--- MinM_API/Dtos/Cart/GetCartItemDto.cs
+++ MinM_API/Dtos/Cart/GetCartItemDto.cs
@@ -3,13 +3,12 @@ using MinM_API.Dtos.ProductVariant;
 
 namespace MinM_API.Dtos.Cart
 {
-    public record GetCartItemDto { 
-        public string Id {get; set;}
-        string productId { get; set; }
-        GetProductDto ChosenProduct { get; set; }
-        string ProductVariantId { get; set; }
-        GetProductVariantDto ChosenVariant { get; set; }
-        int Quantity { get; set; }
-        DateTime AddedAt { get; set; }
+    public record GetCartItemDto 
+    { 
+        public string Id { get; set; }
+        public string ProductId { get; set; }
+        public string ProductVariantId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime AddedAt { get; set; }
     }
 }
