a1863631c1748592c5ae8381597da6ea4a18e2cb
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-07-21 21:31:50 +0200

    Implemented migration for further functionality (#20)

---
+++ MinM_API/Migrations/20250721163758_PhotoBanners.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinM_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PhotoBanners : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BannerImages",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    SequenceNumber = table.Column<int>(type: "int", nullable: false),
+                    URL = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BannerImages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BannerImages");
+        }
+    }
+}
