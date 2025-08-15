f60e631bf30c4e0941ce109f3c8b2ef67b2ff422
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-08-15 20:03:20 +0200

    Added OrderItemMapper for further mapping (#22)

---
+++ MinM_API/Mappers/OrderMapper.cs
@@ -0,0 +1,15 @@
+﻿using MinM_API.Dtos.Order;
+using MinM_API.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace MinM_API.Mappers
+{
+    [Mapper]
+    public partial class OrderItemMapper
+    {
+        [MapperIgnoreSource(nameof(OrderItem.Id))]
+        [MapperIgnoreSource(nameof(OrderItem.Order))]
+        [MapperIgnoreSource(nameof(OrderItem.Item))]
+        public partial OrderItem OrderItemDtoToOrderItem(OrderItemDto orderItemDto);
+    }
+}
