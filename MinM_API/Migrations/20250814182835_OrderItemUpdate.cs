a876fbb025cfe61908811c3ef21ac890b4f6dda8
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-08-15 11:02:57 +0200

    Added new migration for OrderItem and ProductVariant relation

---
+++ MinM_API/Migrations/20250814182835_OrderItemUpdate.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinM_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderItemUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Products_ProductId",
+                table: "OrderItems");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "OrderItems",
+                newName: "ItemId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItems_ProductId",
+                table: "OrderItems",
+                newName: "IX_OrderItems_ItemId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryMethod",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<long>(
+                name: "OrderNumber",
+                table: "Orders",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserEmail",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserFirstName",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserLastName",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserPhone",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_ProductVariants_ItemId",
+                table: "OrderItems",
+                column: "ItemId",
+                principalTable: "ProductVariants",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_ProductVariants_ItemId",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryMethod",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderNumber",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserEmail",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserFirstName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserLastName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UserPhone",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemId",
+                table: "OrderItems",
+                newName: "ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItems_ItemId",
+                table: "OrderItems",
+                newName: "IX_OrderItems_ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Products_ProductId",
+                table: "OrderItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
