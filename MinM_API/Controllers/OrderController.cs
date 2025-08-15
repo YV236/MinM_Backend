b8c2f9858680de09eb7eed70d0203eea9849115e
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-08-15 20:05:04 +0200

    Implemented OrderController (#22)

---
+++ MinM_API/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinM_API.Dtos;
+using MinM_API.Dtos.Order;
+using MinM_API.Services.Interfaces;
+
+namespace MinM_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController(IOrderService orderService) : ControllerBase
+    {
+        [HttpPost]
+        [Route("Create")]
+        public async Task<ActionResult<ServiceResponse<int>>> CreateOrder(AddOrderDto addOrderDto)
+        {
+            var response = await orderService.CreateOrder(addOrderDto, User);
+
+            return StatusCode((int)response.StatusCode, response);
+        }
+    }
+}
