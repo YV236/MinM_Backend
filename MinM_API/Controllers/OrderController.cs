5d7b85ceb9aa4784bda553bd40742eb9145d1cf0
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-08-16 12:23:33 +0200

    Separated order creation for authorized and unauthorized users in controller (#22)

--- MinM_API/Controllers/OrderController.cs
+++ MinM_API/Controllers/OrderController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MinM_API.Dtos;
 using MinM_API.Dtos.Order;
+using MinM_API.Extension;
 using MinM_API.Services.Interfaces;
 
 namespace MinM_API.Controllers
@@ -11,11 +13,21 @@ namespace MinM_API.Controllers
     public class OrderController(IOrderService orderService) : ControllerBase
     {
         [HttpPost]
-        [Route("Create")]
+        [Route("create-authenticated")]
         [Authorize(AuthenticationSchemes = "MyTokenScheme")]
         public async Task<ActionResult<ServiceResponse<int>>> CreateOrder(AddOrderDto addOrderDto)
         {
+
             var response = await orderService.CreateOrder(addOrderDto, User);
+
+            return StatusCode((int)response.StatusCode, response);
+        }
+
+        [HttpPost]
+        [Route("create-guest")]
+        public async Task<ActionResult<ServiceResponse<int>>> CreateUnauthorizedOrder(AddOrderDto addOrderDto)
+        {
+            var response = await orderService.CreateUnauthorizedOrder(addOrderDto);
 
             return StatusCode((int)response.StatusCode, response);
         }
