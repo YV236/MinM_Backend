9ab3d2ecdf63f44b65466c4499668ace30058593
Yurii Vinnyk <yuravinnyk67@gmail.com>
2025-08-23 20:09:09 +0200

    Implemented review service functionality (#23)

--- MinM_API/Services/Implementations/ReviewService.cs
+++ MinM_API/Services/Implementations/ReviewService.cs
@@ -1,34 +1,167 @@
-﻿using MinM_API.Dtos;
+﻿using MinM_API.Data;
+using MinM_API.Dtos;
+using MinM_API.Dtos.Category;
 using MinM_API.Dtos.Review;
+using MinM_API.Extension;
+using MinM_API.Mappers;
+using MinM_API.Models;
+using MinM_API.Repositories.Implementations;
 using MinM_API.Services.Interfaces;
+using System.Security.Claims;
+using System.Net;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace MinM_API.Services.Implementations
 {
-    public class ReviewService : IReviewService
+    public class ReviewService(DataContext context, ProductMapper mapper,
+        ILogger<ProductService> logger, UserRepository userRepository) : IReviewService
     {
-        public Task<ServiceResponse<int>> CreateReview(AddReviewDto request)
+        public async Task<ServiceResponse<int>> CreateReview(AddReviewDto request, ClaimsPrincipal user)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var getUser = await userRepository.FindUser(user, context);
+
+                if (getUser is null)
+                {
+                    return ResponseFactory.Error(0, "User not found", HttpStatusCode.NotFound);
+                }
+
+                var review = new Review
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    UserId = getUser.Id,
+                    ProductId = request.ProductId,
+                    Rating = request.Rating,
+                    Comment = request.Comment,
+                    CreatedAt = DateTime.UtcNow,
+                };
+
+                context.Reviews.Add(review);
+                await context.SaveChangesAsync();
+
+                return ResponseFactory.Success(1, "Review added successfully");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Fail: Error while adding review.");
+                return ResponseFactory.Error(0, "Internal error");
+            }
         }
 
-        public Task<ServiceResponse<int>> DeleteReview(string reviewId)
+        public async Task<ServiceResponse<int>> EditReview(EditReviewDto request, ClaimsPrincipal user)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var getUser = await userRepository.FindUser(user, context);
+
+                if (getUser is null)
+                {
+                    return ResponseFactory.Error(0, "User not found", HttpStatusCode.NotFound);
+                }
+
+                var review = await context.Reviews.FirstOrDefaultAsync(r => r.Id == request.Id && r.UserId == getUser.Id);
+
+                if (review is null)
+                {
+                    return ResponseFactory.Error(0, "Review not found", HttpStatusCode.NotFound);
+                }
+
+                review.Rating = request.Rating;
+                review.Comment = request.Comment;
+
+                await context.SaveChangesAsync();
+
+                return ResponseFactory.Success(1, "Review edited successfully");
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex, "Fail: Error while editing review.");
+                return ResponseFactory.Error(0, "Internal error");
+            }
         }
 
-        public Task<ServiceResponse<List<GetReviewDto>>> GetAllProductReviews()
+        public async Task<ServiceResponse<List<GetReviewDto>>> GetAllProductReviews(string productId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var productReviews = await context.Reviews.Where(r => r.ProductId == productId).ToListAsync();
+
+                if (productReviews.Count == 0)
+                {
+                    return ResponseFactory.Error<List<GetReviewDto>>(null, "Reviews not found", HttpStatusCode.NotFound);
+                }
+
+                var result = new List<GetReviewDto>();
+
+                foreach (var productReview in productReviews)
+                {
+                    result.Add(new GetReviewDto
+                    {
+                        Id = productReview.Id,
+                        Rating = productReview.Rating,
+                        Comment = productReview.Comment,
+                    });
+                }
+
+                return ResponseFactory.Success(result, "Successful extraction of product reviews");
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex, "Fail: Error while retrieving reviews.");
+                return ResponseFactory.Error<List<GetReviewDto>>(null, "Internal error");
+            }
         }
 
-        public Task<ServiceResponse<GetReviewDto>> GetProductReview(string reviewId)
+        public async Task<ServiceResponse<GetReviewDto>> GetProductReview(string reviewId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var productReview = await context.Reviews.FirstOrDefaultAsync(r => r.Id == reviewId);
+
+                if (productReview is null)
+                {
+                    return ResponseFactory.Error<GetReviewDto>(null, "Review not found", HttpStatusCode.NotFound);
+                }
+
+                var result = new GetReviewDto
+                {
+                    Id = productReview.Id,
+                    Rating = productReview.Rating,
+                    Comment = productReview.Comment,
+                };
+
+                return ResponseFactory.Success(result, "Successful extraction of product reviews");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Fail: Error while retrieving reviews.");
+                return ResponseFactory.Error<GetReviewDto>(null, "Internal error");
+            }
         }
 
-        public Task<ServiceResponse<int>> EditReview(EditReviewDto request)
+        public async Task<ServiceResponse<int>> DeleteReview(string reviewId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var productReview = await context.Reviews.FirstOrDefaultAsync(r => r.Id == reviewId);
+
+                if (productReview is null)
+                {
+                    return ResponseFactory.Error(0, "Review not found", HttpStatusCode.NotFound);
+                }
+
+                context.Reviews.Remove(productReview);
+                await context.SaveChangesAsync();
+
+                return ResponseFactory.Success(1, "Successful delete of product review");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Fail: Error while retrieving reviews.");
+                return ResponseFactory.Error(0, "Internal error");
+            }
         }
     }
 }
